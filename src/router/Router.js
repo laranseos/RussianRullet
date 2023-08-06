907caab415d50d0d77114ea02cfd130aaf49bec5
larans7277@gmail.com <larans7277@gmail.com>
2023-08-07 03:51:34 +0900

    fix update2

--- src/router/Router.js
+++ src/router/Router.js
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom'
 import MainPage from "../pages/MainPage";
 import Autoplay from "../pages/Autoplay";
 import Test from "../pages/Test"
+import Test1 from "../pages/Test1";
 
 const Routers = () => {
     return (
@@ -10,6 +11,8 @@ const Routers = () => {
             <Route path='/' element={<MainPage />}/>
             <Route path='/autoplay' element={<Autoplay />} />
             <Route path='/start' element={<Test />}/>
+            <Route path='/test1' element={<Test1 />}/>
+
         </Routes>
     )
 }
